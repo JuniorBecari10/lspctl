9610d56bd2663cdf021a2894eaa3eda657bd53a9
Antônio Carlos <antonioocarlos@proton.me>
2026-08-09 19:08:04 -0300

    feat: clean tmp fir

--- src/root.rs
+++ src/root.rs
@@ -16,11 +16,14 @@ pub fn setup_root() -> anyhow::Result<()> {
     }
 }
 
-// TODO: delete everything in tmp here and add lockfile
-// to ensure no more than one instance of lspctl runs at once.
+// TODO: add lockfile to ensure no more than one instance of lspctl runs at once.
 // also delete all folders in packages that isn't in registry.
-// basically the recover command.
+// basically the recover command. maybe make this a manual operation.
+// this assumes no other instance is running, so we need to add checks!
 fn ensure_root_items() -> anyhow::Result<()> {
+    // clean tmp dir. it's created below
+    fs::remove_dir_all(paths::tmp_dir())?;
+
     fs::create_dir_all(paths::bin_dir())?;
     fs::create_dir_all(paths::tmp_dir())?;
     fs::create_dir_all(paths::registry_dir())?;
